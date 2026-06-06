5d1a15f0f1d0feee5cbf68003e241b5976aebb41
prabhatrai-debug <prabhatrai1515@gmail.com>
2026-06-06 22:58:33 +0530

    Add CNN model

---
+++ TrainModel.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers, models
+
+IMG_SIZE = 128
+BATCH_SIZE = 32
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2
+)
+
+train_generator = train_datagen.flow_from_directory(
+    "C:/Users/LOQ/Desktop/Prabhat Rai/Face Mask Detection/data",
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode="binary",
+    subset="training"
+)
+
+val_generator = train_datagen.flow_from_directory(
+    "C:/Users/LOQ/Desktop/Prabhat Rai/Face Mask Detection/data",
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode="binary",
+    subset="validation"
+)
+
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu',
+                  input_shape=(IMG_SIZE, IMG_SIZE, 3)),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(128, (3,3), activation='relu'),
+    layers.MaxPooling2D(),
+
+    layers.Flatten(),
+
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.5),
+
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(
+    train_generator,
+    validation_data=val_generator,
+    epochs=10
+)
+
+model.save("Models/mask_detector.keras")
